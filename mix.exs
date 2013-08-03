8fa85352f8bdf05f466a931f07fe64d6a29b8993
Christoph Grabo <chris@dinarrr.com>
2013-08-03 17:33:24 +0200

    Shorten dependency declaration

--- mix.exs
+++ mix.exs
@@ -19,6 +19,6 @@ defmodule Gru.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :minion, git: "https://github.com/BananaLtd/minion.git" } ]
+    [ { :minion, github: "BananaLtd/minion" } ]
   end
 end
